7a46ba923a2387700ae03984c5b90965ff183ade
Aiden <0@0>
2021-03-29 04:26:01 +0800

    Introduce React router

--- src/web/src/index.js
+++ src/web/src/index.js
@@ -1,3 +1,31 @@
-import React from "react";
+import App from './pages/App';
+import Login from './pages/Login';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
-console.log("Hello Webpack");
+
+ReactDOM.render((
+    <Router>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+            <Route exact path="/">
+                <App/>
+            </Route>
+            <Route path="/login">
+                <Login/>
+            </Route>
+        </Switch>
+    </Router>
+), document.body);
---
+++ src/web/src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Login extends React.PureComponent {
+    render() {
+        return (
+            <div>Hello I'm login</div>
+        )
+    }
+}

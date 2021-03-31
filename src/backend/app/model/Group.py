46d56600b3c8ca53c8f48d417b6f9d8b064f3461
Deleted User <16668056+nobody-65534@users.noreply.github.com>
2021-04-01 02:50:13 +0800

    fix user group relation

--- src/backend/app/model/Group.py
+++ src/backend/app/model/Group.py
@@ -7,11 +7,10 @@ class Group(db.Model):
     uuid = db.Column("uuid", db.BINARY(16), primary_key=True)   # PK
     creation_time = db.Column("creation_time", db.Integer, nullable=False)
     creator_uuid = db.Column("creator_uuid", db.BINARY(16), db.ForeignKey("user.uuid"), nullable=False) # FK
-    r_user = db.relationship("User", backref="created_group", lazy=True, uselist=False)
     name = db.Column("name", db.String(256), nullable=False)
     description = db.Column("description", db.String(4096), nullable=True)
     proposal = db.Column("proposal", db.String(4096), nullable=True)
     group_state = db.Column("state", db.String(256), nullable=False)
 
     def __repr__(self):
-        return f"<Group {self.uuid}: {self.name}>"
+        return f"<Group {self.uuid.hex()}: {self.name}>"

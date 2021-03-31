ca3ce11f7fac622b3fcd5ebb5f2eb70840f75a02
4piu <16668056+4piu@users.noreply.github.com>
2021-04-01 05:33:52 +0800

    change use of word

--- src/backend/app/model/Group.py
+++ src/backend/app/model/Group.py
@@ -6,11 +6,12 @@ class Group(db.Model):
 
     uuid = db.Column("uuid", db.BINARY(16), primary_key=True)   # PK
     creation_time = db.Column("creation_time", db.Integer, nullable=False)
-    creator_uuid = db.Column("creator_uuid", db.BINARY(16), db.ForeignKey("user.uuid"), nullable=False) # FK
+    owner_uuid = db.Column("owner_uuid", db.BINARY(16), db.ForeignKey("user.uuid"), nullable=False) # FK
     name = db.Column("name", db.String(256), nullable=False)
     description = db.Column("description", db.String(4096), nullable=True)
     proposal = db.Column("proposal", db.String(4096), nullable=True)
     proposal_state = db.Column("proposal_state", db.String(256), nullable=False) # PENDING/SUBMITTED/SUBMITTED_LATE
+    application_enabled = db.Column("application_enabled", db.Boolean(), default=True)
 
     def __repr__(self):
         return f"<Group {self.uuid.hex()}: {self.name}>"

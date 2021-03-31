35679d6759cc2bae52b9d26ae517112f0b1101f2
Deleted User <16668056+nobody-65534@users.noreply.github.com>
2021-04-01 05:12:59 +0800

    add proposal state to group model

--- src/backend/app/model/Group.py
+++ src/backend/app/model/Group.py
@@ -10,7 +10,7 @@ class Group(db.Model):
     name = db.Column("name", db.String(256), nullable=False)
     description = db.Column("description", db.String(4096), nullable=True)
     proposal = db.Column("proposal", db.String(4096), nullable=True)
-    group_state = db.Column("state", db.String(256), nullable=False)
+    proposal_state = db.Column("proposal_state", db.String(256), nullable=False) # PENDING/SUBMITTED/SUBMITTED_LATE
 
     def __repr__(self):
         return f"<Group {self.uuid.hex()}: {self.name}>"

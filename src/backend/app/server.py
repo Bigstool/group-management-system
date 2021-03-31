afde5dd16794ab136ee3f34d25395196b738110c
4piu <16668056+4piu@users.noreply.github.com>
2021-04-01 02:32:09 +0800

    group api docs

--- src/backend/app/server.py
+++ src/backend/app/server.py
@@ -7,6 +7,7 @@ from flask import Flask, request, g
 from werkzeug.exceptions import HTTPException
 
 from blueprint.auth_api import auth_api
+from blueprint.group_api import group_api
 from blueprint.user_api import user_api
 from shared import config, get_logger, db
 from utility.ApiException import ApiException
@@ -60,6 +61,7 @@ def err_handler(e):
 # APIs
 app.register_blueprint(auth_api)
 app.register_blueprint(user_api)
+app.register_blueprint(group_api)
 
 # SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = \

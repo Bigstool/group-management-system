2f116a83762ea82486a3aec9a82d44241ab7a9c3
Deleted User <16668056+nobody-65534@users.noreply.github.com>
2021-04-01 05:33:52 +0800

    change use of word

--- src/backend/app/model/User.py
+++ src/backend/app/model/User.py
@@ -11,7 +11,7 @@ class User(db.Model):
     bio = db.Column("bio", db.Text, nullable=True)
     password_salt = db.Column("password_salt", db.BINARY(16), nullable=False)
     password_hash = db.Column("password_hash", db.BINARY(20), nullable=False)
-    r_group = db.relationship("Group", backref="creator", lazy=True, uselist=False)
+    r_group = db.relationship("Group", backref="owner", lazy=True, uselist=False)
     r_group_comment = db.relationship("GroupComment", backref="author", lazy=True, uselist=False)
     r_group_application = db.relationship("GroupApplication", backref="applicant", lazy=True, uselist=False)
 

214e4c3bd2e216dcaeacf674972bdb6ee788de30
Lullaby <67684842+Leolando@users.noreply.github.com>
2021-04-06 13:30:04 +0800

    Update model, complete 4 API
    Update the relationship between User and Group,  complete API: get_user_profile, update_group_info, get_group_application_list, get_user_application_list

--- src/backend/app/model/User.py
+++ src/backend/app/model/User.py
@@ -11,9 +11,14 @@ class User(db.Model):
     bio = db.Column("bio", db.Text, nullable=True)
     password_salt = db.Column("password_salt", db.BINARY(16), nullable=False)
     password_hash = db.Column("password_hash", db.BINARY(20), nullable=False)
-    r_group = db.relationship("Group", backref="owner", lazy=True, uselist=False)
-    r_group_comment = db.relationship("GroupComment", backref="author", lazy=True, uselist=False)
-    r_group_application = db.relationship("GroupApplication", backref="applicant", lazy=True, uselist=False)
+    # r_group = db.relationship("Group", backref="owner", lazy=True, uselist=False)
+    r_group_comment = db.relationship("GroupComment", backref="author", lazy=True)
+    r_group_application = db.relationship("GroupApplication", backref="applicant", lazy=True)
+
+    #change
+    group_id = db.Column("group_id", db.BINARY(16), db.ForeignKey("group.uuid"))
+    r_group_favorite = db.relationship("GroupFavorite", backref="follower", lazy=True)
+    group = db.relationship("Group", backref ="member", lazy=True)
 
     def __repr__(self):
         return f"<User {self.uuid.hex()}: {self.email}>"

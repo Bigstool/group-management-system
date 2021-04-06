ccba10e4e636832a6b2eefb6a525b6d81fb7660d
Deleted User <16668056+nobody-65534@users.noreply.github.com>
2021-04-07 04:03:23 +0800

    AuthProvider and PrivateRoute class

--- src/web/src/pages/Login.js
+++ src/web/src/pages/Login.js
@@ -3,58 +3,64 @@ import {Row, Col} from 'antd';
 import {Form, Input, Button, Checkbox} from 'antd';
 import {Card} from 'antd';
 import './Login.scss';
+import {AuthContext} from "../utilities/AuthProvider";
+import {Redirect} from "react-router-dom";
 
 export default class Login extends React.PureComponent {
-    render() {
+    static contextType = AuthContext;
 
-        const layout = {
-            labelCol: {span: 8},
-            wrapperCol: {span: 16},
-        };
-        const tailLayout = {
-            wrapperCol: {offset: 8, span: 16},
-        };
+    render() {
+        // TODO redirect user if already logged in
+        if (this.context.user) {
+            return <Redirect to={{pathname: "/"}}/>
+        }
 
         const onFinish = (values) => {
+            let login_success = this.context.login();
             console.log('Success:', values);
         };
 
         const onFinishFailed = (errorInfo) => {
             console.log('Failed:', errorInfo);
         };
+
         return (
-            <Row className={"LoginForm"} align="middle" justify="center">
+            <Row className={"LoginForm"}
+                 align="middle"
+                 justify="center">
                 <Col>
                     <Card title={"Login"}>
-                        <Form
-                            {...layout}
-                            name="basic"
-                            initialValues={{remember: true}}
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
+                        <Form labelCol={{span: 8}}
+                              wrapperCol={{span: 16}}
+                              name="basic"
+                              initialValues={{remember: true}}
+                              onFinish={onFinish}
+                              onFinishFailed={onFinishFailed}
                         >
-                            <Form.Item
-                                label="E-mail"
-                                name="email"
-                                rules={[{required: true, message: 'Please input your e-mail!'}]}
+                            <Form.Item label="E-mail"
+                                       name="email"
+                                       rules={[{required: true, message: 'Please input your e-mail!'}]}
                             >
                                 <Input/>
                             </Form.Item>
 
-                            <Form.Item
-                                label="Password"
-                                name="password"
-                                rules={[{required: true, message: 'Please input your password!'}]}
+                            <Form.Item label="Password"
+                                       name="password"
+                                       rules={[{required: true, message: 'Please input your password!'}]}
                             >
                                 <Input.Password/>
                             </Form.Item>
 
-                            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                            <Form.Item wrapperCol={{offset: 8, span: 16}}
+                                       name="remember"
+                                       valuePropName="checked">
                                 <Checkbox>Remember me</Checkbox>
                             </Form.Item>
 
-                            <Form.Item {...tailLayout}>
-                                <Button type="primary" htmlType="submit">
+                            <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                                <Button
+                                    type="primary"
+                                    htmlType="submit">
                                     Login
                                 </Button>
                             </Form.Item>
---
+++ src/web/src/utilities/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Route, Redirect} from "react-router-dom";
+import {AuthContext} from "./AuthProvider";
+
+export default class PrivateRoute extends React.PureComponent {
+    static contextType = AuthContext;
+
+    render() {
+        const {children, ...rest} = this.props;
+        return (
+            <Route
+                {...rest}
+                render={(props) => {
+                    return this.context.user === null ? (
+                        <Redirect to={{
+                            pathname: "/login",
+                            state: {from: props.location}
+                        }}/>
+                    ) : (children)
+                }}
+            />
+        );
+    }
+}

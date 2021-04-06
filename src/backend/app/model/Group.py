89985916c7f22e138203f90d99d68a642c21959c
Lullaby <67684842+Leolando@users.noreply.github.com>
2021-04-06 13:30:04 +0800

    Update model, complete 4 API
    Update the relationship between User and Group,  complete API: get_user_profile, update_group_info, get_group_application_list, get_user_application_list

--- src/backend/app/model/Group.py
+++ src/backend/app/model/Group.py
@@ -6,12 +6,16 @@ class Group(db.Model):
 
     uuid = db.Column("uuid", db.BINARY(16), primary_key=True)   # PK
     creation_time = db.Column("creation_time", db.Integer, nullable=False)
-    owner_uuid = db.Column("owner_uuid", db.BINARY(16), db.ForeignKey("user.uuid"), nullable=False) # FK
+
     name = db.Column("name", db.String(256), nullable=False)
     description = db.Column("description", db.String(4096), nullable=True)
     proposal = db.Column("proposal", db.String(4096), nullable=True)
     proposal_state = db.Column("proposal_state", db.String(256), nullable=False) # PENDING/SUBMITTED/SUBMITTED_LATE
     application_enabled = db.Column("application_enabled", db.Boolean(), default=True)
+
+    #change
+    owner_uuid = db.Column("owner_uuid", db.BINARY(16), nullable=False)  # FK
+    member_num = db.Column('member_num', db.Integer, nullable=False, default=1)
 
     def __repr__(self):
         return f"<Group {self.uuid.hex()}: {self.name}>"

32c6f7daed019329707028c256df7c8e5cb1c8b6
4piu <16668056+4piu@users.noreply.github.com>
2021-04-07 05:08:42 +0800

    Add Helmet

--- src/web/src/pages/Login.js
+++ src/web/src/pages/Login.js
@@ -5,6 +5,7 @@ import {Card} from 'antd';
 import './Login.scss';
 import {AuthContext} from "../utilities/AuthProvider";
 import {Redirect} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 export default class Login extends React.PureComponent {
     static contextType = AuthContext;
@@ -25,49 +26,54 @@ export default class Login extends React.PureComponent {
         };
 
         return (
-            <Row className={"LoginForm"}
-                 align="middle"
-                 justify="center">
-                <Col>
-                    <Card title={"Login"}>
-                        <Form labelCol={{span: 8}}
-                              wrapperCol={{span: 16}}
-                              name="basic"
-                              initialValues={{remember: true}}
-                              onFinish={onFinish}
-                              onFinishFailed={onFinishFailed}
-                        >
-                            <Form.Item label="E-mail"
-                                       name="email"
-                                       rules={[{required: true, message: 'Please input your e-mail!'}]}
+            <>
+                <Helmet>
+                    <title>GMS - Login</title>
+                </Helmet>
+                <Row className={"LoginForm"}
+                     align="middle"
+                     justify="center">
+                    <Col>
+                        <Card title={"Login"}>
+                            <Form labelCol={{span: 8}}
+                                  wrapperCol={{span: 16}}
+                                  name="basic"
+                                  initialValues={{remember: true}}
+                                  onFinish={onFinish}
+                                  onFinishFailed={onFinishFailed}
                             >
-                                <Input/>
-                            </Form.Item>
+                                <Form.Item label="E-mail"
+                                           name="email"
+                                           rules={[{required: true, message: 'Please input your e-mail!'}]}
+                                >
+                                    <Input/>
+                                </Form.Item>
 
-                            <Form.Item label="Password"
-                                       name="password"
-                                       rules={[{required: true, message: 'Please input your password!'}]}
-                            >
-                                <Input.Password/>
-                            </Form.Item>
+                                <Form.Item label="Password"
+                                           name="password"
+                                           rules={[{required: true, message: 'Please input your password!'}]}
+                                >
+                                    <Input.Password/>
+                                </Form.Item>
 
-                            <Form.Item wrapperCol={{offset: 8, span: 16}}
-                                       name="remember"
-                                       valuePropName="checked">
-                                <Checkbox>Remember me</Checkbox>
-                            </Form.Item>
+                                <Form.Item wrapperCol={{offset: 8, span: 16}}
+                                           name="remember"
+                                           valuePropName="checked">
+                                    <Checkbox>Remember me</Checkbox>
+                                </Form.Item>
 
-                            <Form.Item wrapperCol={{offset: 8, span: 16}}>
-                                <Button
-                                    type="primary"
-                                    htmlType="submit">
-                                    Login
-                                </Button>
-                            </Form.Item>
-                        </Form>
-                    </Card>
-                </Col>
-            </Row>
+                                <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                                    <Button
+                                        type="primary"
+                                        htmlType="submit">
+                                        Login
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </Card>
+                    </Col>
+                </Row>
+            </>
         );
     }
 }

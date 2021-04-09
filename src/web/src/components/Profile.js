3965a6ec76fbcab3a9549caae923249f18ee7107
Zeusandrea <80686025+Zeusandrea@users.noreply.github.com>
2021-04-09 13:14:22 +0800

    Profile static page first draft

--- src/web/src/components/Profile.js
+++ src/web/src/components/Profile.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import "./Profile.scss";
+import { Button } from 'antd';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+// import "./Profile.scss";
 
 export default class Profile extends React.PureComponent {
     render() {
         return (
-            <div>
-                Profile fragment
-            </div>
-        )
+            <>
+                <div>
+                    <Avatar size={64} icon={<UserOutlined/>}/>
+                </div>
+                <Button id='editProfile' type='primary'>
+                    Edit Profile
+                </Button>
+                <Button id='changePassword' type='primary'>
+                    Change Password
+                </Button>
+                <Button id='createGroup' type='primary'>
+                    Create Group
+                </Button>
+                <Button id='semesterTools' type='primary'>
+                    Semester Tools
+                </Button>
+                <Button id='reports' type='primary'>
+                    Reports
+                </Button>
+                <Button id='archives' type='primary'>
+                    Archives
+                </Button>
+            </>
+        );
     }
 }

eae2ebfbec729e6fee976abb13d8dd38ef7021f4
4piu <16668056+4piu@users.noreply.github.com>
2021-04-12 07:38:34 +0800

    GroupCard

--- src/web/src/components/GroupCard.js
+++ src/web/src/components/GroupCard.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import {Card} from "antd";
+import {UsergroupAddOutlined} from "@ant-design/icons";
+
 export default class GroupCard extends React.PureComponent {
+    static propTypes = {
+        groupItem: PropTypes.object.isRequired
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <>
-                <div>Group Card</div>
-            </>
+            <Card>
+                <Card.Meta
+                    title={this.props["groupItem"]["name"]}
+                    description={this.props["groupItem"]["description"]}
+                />
+            </Card>
         )
     }
 }

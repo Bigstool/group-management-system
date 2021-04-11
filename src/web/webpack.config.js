45ff567d3896caca0c7a019ffd5f71a42059d90f
Deleted User <16668056+nobody-65534@users.noreply.github.com>
2021-04-12 06:29:43 +0800

    Use @boundMethod decorator

--- src/web/webpack.config.js
+++ src/web/webpack.config.js
@@ -30,8 +30,10 @@ module.exports = {
                                 "@babel/preset-react"
                             ],
                             plugins: [
+                                ["@babel/plugin-proposal-decorators", { "legacy": true }],
                                 "@babel/plugin-proposal-class-properties",
-                                "@babel/plugin-transform-runtime"
+                                "@babel/plugin-transform-runtime",
+
                             ]
                         }
                     }

1088240df6411993c27f1498d5d5dc396c7a2318
Bigstool <20697772+Bigstool@users.noreply.github.com>
2021-04-24 17:12:11 +0800

    Add keys to the components in the danger zone array

--- src/web/src/pages/GroupConfig.js
+++ src/web/src/pages/GroupConfig.js
@@ -75,34 +75,57 @@ export default class GroupConfig extends React.Component {
 
     // Caution Zone
     let cautionZone = [];
-    cautionZone.push(<Divider className={'caution-zone'} orientation="center" plain>Caution Zone</Divider>);
+    cautionZone.push(
+      <Divider className={'caution-zone'} orientation="center" plain key={'divider'}>
+        Caution Zone
+      </Divider>
+    );
     // Leave Group (Member) (Before Grouping DDL)
     if (isMember && !afterGroupingDDL) {
-      cautionZone.push(<Button danger block size={'large'}>Leave Group</Button>);
+      cautionZone.push(
+        <Button danger block size={'large'} key={'leave-group'}>
+          Leave Group
+        </Button>
+      );
     }
     // Full (Group Owner) (Before Grouping DDL) (Available when Member Limits Reached)
+    // TODO: disable the button when not enough group members
     if (isOwner && !afterGroupingDDL) {
       cautionZone.push(
-        <Button danger block size={'large'}>{isFull ? `Unmark Group as Full` : `Mark Group as Full`}</Button>
+        <Button danger block size={'large'} key={'full'}>
+          {isFull ? `Unmark Group as Full` : `Mark Group as Full`}
+        </Button>
       );
     }
     // Submit (Group Owner) (After Grouping DDL) (Before Approve)
     if (isOwner && afterGroupingDDL && !isApproved) {
       cautionZone.push(
-        <Button danger block size={'large'}>{isSubmitted ? 'Revert Submission' : 'Submit Project for Approval'}</Button>
+        <Button danger block size={'large'} key={'submit'}>
+          {isSubmitted ? 'Revert Submission' : 'Submit Project for Approval'}
+        </Button>
       );
     }
     // Approve (Admin) (After Proposal DDL) (Submitted)
     if (isAdmin && afterProposalDDL && isSubmitted) {
-      cautionZone.push(<Button danger block size={'large'}>Approve Proposal</Button>);
+      cautionZone.push(
+        <Button danger block size={'large'} key={'approve'}>
+          Approve Proposal
+        </Button>
+      );
     }
     // Transfer Ownership (Group Owner or Admin) (All stages)
     if (isOwner || isAdmin) {
-      cautionZone.push(<Button danger block size={'large'}>Transfer Group Owner</Button>);
+      cautionZone.push(<Button danger block size={'large'} key={'transfer-ownership'}>
+        Transfer Group Owner
+      </Button>);
     }
     // Dismiss Group (Group Owner) (Before Grouping DDL) or (Admin) (Before Proposal DDL)
     if ((isOwner && !afterGroupingDDL) || (isAdmin && !afterProposalDDL)) {
-      cautionZone.push(<Button danger block size={'large'}>Dismiss Group</Button>);
+      cautionZone.push(
+        <Button danger block size={'large'} key={'dismiss-group'}>
+          Dismiss Group
+        </Button>
+      );
     }
 
     return (

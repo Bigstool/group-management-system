41d9dbd9f6b78ae45547daa555ff950371b9e170
Bigstool <20697772+Bigstool@users.noreply.github.com>
2021-04-25 09:41:34 +0800

    Introduce state

--- src/web/src/pages/GroupConfig.js
+++ src/web/src/pages/GroupConfig.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Button, Divider} from 'antd';
+import {LoadingOutlined} from '@ant-design/icons';
 import {boundMethod} from "autobind-decorator";
 import AppBar from "../components/AppBar";
 import './GroupConfig.scss';
+import {AuthContext} from "../utilities/AuthProvider";
 
 /* Bigstool's class notations
 *  #T: Top-level component
@@ -14,28 +16,124 @@ import './GroupConfig.scss';
 
 // #T
 export default class GroupConfig extends React.Component {
-  static propType = {
-    // user list
-    'userList': PropTypes.array,  // TODO: isRequired
+  static propTypes = {
+    // null
+  }
+  static contextType = AuthContext;
+
+  constructor(props, context) {
+    super(props, context);
+    this.userUuid = this.context.getUser()['uuid'];
+    this.userRole = this.context.getUser()['role'];
+    this.groupingDDL = 1618054160  // TODO: get from context (wait for implementation)
+    this.proposalDDL = 1618054160  // TODO: get from context (wait for implementation)
+    this.state = {
+      // User related
+      'isMember': false,
+      'isOwner': false,
+      'isAdmin': false,
+      // System related
+      'afterGroupingDDL': false,
+      'afterProposalDDL': false,
+      // Group related
+      'isApproved': false,
+      'isSubmitted': false,
+      'isFull': false,
+      // Component related
+      'loading': true,
+      'error': false
+    }
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      // TODO: initial state
-    }
+  async componentDidMount() {
+    await this.checkGroupInfo();
+    this.setState({'loading': false});
+  }
 
+  // Retrieves group info and updates this.state
+  @boundMethod
+  async checkGroupInfo() {
+    try {
+      let res = await this.context.request({
+        url: `/group/${this.props.match.params["uuid"]}`,
+        method: 'get'
+      });
+      let groupInfo = res.data['data'];
+
+      // update isMember
+      let found = false;
+      for (let i = 0; i < groupInfo['member'].length; i++) {
+        if (groupInfo['member'][i]['uuid'] === this.userUuid) {
+          found = true;
+          break;
+        }
+      }
+      if (found) this.setState({'isMember': true});
+      else this.setState({'isMember': false});
+
+      // update isOwner
+      if (groupInfo['owner']['uuid'] === this.userUuid) this.setState({'isOwner': true});
+      else this.setState({'isOwner': false});
+
+      // update isAdmin
+      if (this.userRole === 'ADMIN') this.setState({'isAdmin': true});
+      else this.setState({'isAdmin': false});
+
+      // update afterGroupingDDL
+      if (Date.now() > this.groupingDDL) this.setState({'afterGroupingDDL': true});
+      else this.setState({'afterGroupingDDL': false});
+
+      // update afterProposalDDL
+      if (Date.now() > this.proposalDDL) this.setState({'afterProposalDDL': true});
+      else this.setState({'afterProposalDDL': false});
+
+      // update isApproved
+      if (groupInfo['proposal_state'] === 'APPROVED') this.setState({'isApproved': true});
+      else this.setState({'isApproved': false});
+
+      // update isSubmitted
+      if (groupInfo['proposal_state'] === 'SUBMITTED') this.setState({'isSubmitted': true});
+      else this.setState({'isSubmitted': false});
+
+      // update isFull
+      if (!groupInfo['application_enabled']) this.setState({'isFull': true});
+      else this.setState({'isFull': false});
+
+    } catch (error) {
+      this.setState({
+        'error': true
+      });
+    }
   }
 
   render() {
-    let isMember = false, isOwner = true, isAdmin = false;
-    let afterGroupingDDL = false, afterProposalDDL = false;
-    let isApproved = false, isSubmitted = false, isFull = true;
+    // App Bar
+    let appBar = <AppBar/>;
+
+    // TODO: loading and error handling
+    if (this.state.error) {
+      return (
+        <React.Fragment>
+          {appBar}
+          <h1>Oops, something went wrong</h1>
+          <h3>Please try reloading</h3>
+        </React.Fragment>
+      );
+    }
+
+    if (this.state.loading) {
+      return (
+        <React.Fragment>
+          {appBar}
+          <LoadingOutlined/>
+        </React.Fragment>
+      );
+    }
 
     // All Set (Member) (After Grouping DDL)
-    if (isMember && afterGroupingDDL) return (
+    if (this.state.isMember && this.state.afterGroupingDDL) return (
       <React.Fragment>
-        <AppBar/>
+        {appBar}
         <h1>You're all set!</h1>
         <h3>Good luck with your project :)</h3>
       </React.Fragment>
@@ -43,7 +141,7 @@ export default class GroupConfig extends React.Component {
 
     // Group Profile (Group Owner and Admin) (All stages)
     let profile = null;
-    if (isOwner || isAdmin) {
+    if (this.state.isOwner || this.state.isAdmin) {
       profile = <React.Fragment>
         <Button type={'primary'} block size={'large'}>Edit Group Profile</Button>
         <br/>
@@ -53,7 +151,7 @@ export default class GroupConfig extends React.Component {
 
     // Member Management (Group Owner) (Before Grouping DDL)
     let memberManagement = null;
-    if (isOwner && !afterGroupingDDL) {
+    if (this.state.isOwner && !this.state.afterGroupingDDL) {
       memberManagement = <React.Fragment>
         <Button block size={'large'}>Manage Group Members</Button>
         <Button block size={'large'}>Manage Join Applications</Button>
@@ -65,7 +163,7 @@ export default class GroupConfig extends React.Component {
 
     // DELAYED: Showcase (Group Owner) (After Proposal DDL)
     let showcase = null;
-    if (isOwner && afterProposalDDL) {
+    if (this.state.isOwner && this.state.afterProposalDDL) {
       showcase = <React.Fragment>
         <Button block size={'large'}>Manage Showcase</Button>
         <br/>
@@ -81,7 +179,7 @@ export default class GroupConfig extends React.Component {
       </Divider>
     );
     // Leave Group (Member) (Before Grouping DDL)
-    if (isMember && !afterGroupingDDL) {
+    if (this.state.isMember && !this.state.afterGroupingDDL) {
       cautionZone.push(
         <Button danger block size={'large'} key={'leave-group'}>
           Leave Group
@@ -90,23 +188,23 @@ export default class GroupConfig extends React.Component {
     }
     // Full (Group Owner) (Before Grouping DDL) (Available when Member Limits Reached)
     // TODO: disable the button when not enough group members
-    if (isOwner && !afterGroupingDDL) {
+    if (this.state.isOwner && !this.state.afterGroupingDDL) {
       cautionZone.push(
         <Button danger block size={'large'} key={'full'}>
-          {isFull ? `Unmark Group as Full` : `Mark Group as Full`}
+          {this.isFull ? `Unmark Group as Full` : `Mark Group as Full`}
         </Button>
       );
     }
     // Submit (Group Owner) (After Grouping DDL) (Before Approve)
-    if (isOwner && afterGroupingDDL && !isApproved) {
+    if (this.state.isOwner && this.state.afterGroupingDDL && !this.state.isApproved) {
       cautionZone.push(
         <Button danger block size={'large'} key={'submit'}>
-          {isSubmitted ? 'Revert Submission' : 'Submit Project for Approval'}
+          {this.isSubmitted ? 'Revert Submission' : 'Submit Project for Approval'}
         </Button>
       );
     }
     // Approve (Admin) (After Proposal DDL) (Submitted)
-    if (isAdmin && afterProposalDDL && isSubmitted) {
+    if (this.state.isAdmin && this.state.afterProposalDDL && this.state.isSubmitted) {
       cautionZone.push(
         <Button danger block size={'large'} key={'approve'}>
           Approve Proposal
@@ -114,13 +212,13 @@ export default class GroupConfig extends React.Component {
       );
     }
     // Transfer Ownership (Group Owner or Admin) (All stages)
-    if (isOwner || isAdmin) {
+    if (this.state.isOwner || this.state.isAdmin) {
       cautionZone.push(<Button danger block size={'large'} key={'transfer-ownership'}>
         Transfer Group Owner
       </Button>);
     }
     // Dismiss Group (Group Owner) (Before Grouping DDL) or (Admin) (Before Proposal DDL)
-    if ((isOwner && !afterGroupingDDL) || (isAdmin && !afterProposalDDL)) {
+    if ((this.state.isOwner && !this.state.afterGroupingDDL) || (this.state.isAdmin && !this.state.afterProposalDDL)) {
       cautionZone.push(
         <Button danger block size={'large'} key={'dismiss-group'}>
           Dismiss Group
@@ -130,7 +228,7 @@ export default class GroupConfig extends React.Component {
 
     return (
       <React.Fragment>
-        <AppBar/>
+        {appBar}
         <div className={'group-config'}>
           {profile}
           {memberManagement}

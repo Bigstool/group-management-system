cc67769bebd7b623c07aab26d781461e1ff8c0fb
Ethan <minghao.du18@student.xjtlu.edu.cn>
2021-04-25 12:02:53 +0800

    Delete the 'state' attribute of GroupApplication

--- src/backend/app/model/GroupApplication.py
+++ src/backend/app/model/GroupApplication.py
@@ -9,5 +9,4 @@ class GroupApplication(db.Model):
     applicant_uuid = db.Column("applicant_uuid", db.BINARY(16), db.ForeignKey("user.uuid"), nullable=False)
     group_uuid = db.Column("group_uuid", db.BINARY(16), db.ForeignKey("group.uuid"), nullable=False)
     comment = db.Column("comment", db.String(4096), nullable=True)
-    state = db.Column("state", db.String(256), nullable=False)
     r_group = db.relationship("Group", backref="application", lazy=True)

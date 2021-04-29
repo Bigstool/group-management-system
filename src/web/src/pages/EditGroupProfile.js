1f8d857dd969a3f24764550882dd4f71cfe556ef
Bigstool <20697772+Bigstool@users.noreply.github.com>
2021-04-29 21:46:52 +0800

    Connect to backend

--- src/web/src/pages/EditGroupProfile.js
+++ src/web/src/pages/EditGroupProfile.js
@@ -6,6 +6,7 @@ import {boundMethod} from "autobind-decorator";
 import AppBar from "../components/AppBar";
 import './EditGroupProfile.scss';
 import {AuthContext} from "../utilities/AuthProvider";
+import {Redirect} from "react-router-dom";
 
 /* Bigstool's class notations
 *  #T: Top-level component
@@ -27,9 +28,10 @@ export default class AccountProfile extends React.Component {
       // User related
       userUuid: this.context.getUser()['uuid'],
       userRole: this.context.getUser()['role'],
+      isOwner: false,
       isAdmin: false,
       // Group related
-      groupUuid: '',  // TODO: this.props.match.params["uuid"]
+      groupUuid: this.props.match.params["uuid"],
       name: '',
       title: '',
       description: '',
@@ -45,6 +47,67 @@ export default class AccountProfile extends React.Component {
       loading: true,
       error: false,
       redirect: false,
+      push: false,
+      // Event related
+      saving: false,
+    }
+  }
+
+  async componentDidMount() {
+    await this.checkSysConfig();
+    await this.checkGroupInfo();
+    // Check whether user has permission to access this page
+    if (!this.state.isOwner && !this.state.isAdmin) {
+      this.setState({
+        'redirect': '/',
+        'push': false,
+      });
+    }
+    this.setState({loading: false});
+  }
+
+  // Retrieves system config and updates this.state
+  @boundMethod
+  async checkSysConfig() {
+    let sysConfig = await this.context.getSysConfig();
+    this.setState({
+      groupingDDL: sysConfig["system_state"]["grouping_ddl"],
+    });
+    // update afterGroupingDDL
+    if (Date.now() > this.state.groupingDDL) this.setState({afterGroupingDDL: true});
+    else this.setState({afterGroupingDDL: false});
+  }
+
+  // Retrieves group info and updates this.state
+  @boundMethod
+  async checkGroupInfo() {
+    try {
+      let res = await this.context.request({
+        path: `/group/${this.state.groupUuid}`,
+        method: 'get'
+      });
+      let groupInfo = res.data['data'];
+
+      // update name, title, description, proposal
+      this.setState({
+        name: groupInfo['name'],
+        title: groupInfo['title'],
+        description: groupInfo['description'],
+        proposal: groupInfo['proposal'],
+      })
+
+      // update isOwner
+      if (groupInfo['owner']['uuid'] === this.state.userUuid) this.setState({isOwner: true});
+      else this.setState({isOwner: false});
+
+      // update isAdmin
+      if (this.state.userRole === 'ADMIN') this.setState({isAdmin: true});
+      else this.setState({isAdmin: false});
+
+    } catch (error) {
+      this.setState({
+        'error': true
+      });
     }
   }
 
@@ -68,10 +131,71 @@ export default class AccountProfile extends React.Component {
     this.setState({proposal: event.target.value});
   }
 
-  // TODO: check whether user has permission to access this page: role and stage
+  @boundMethod
+  async onSave() {
+    this.setState({saving: true});
+    try {
+      await this.context.request({
+        path: `/group/${this.state.groupUuid}`,
+        method: "patch",
+        data: {
+          name: this.state.name,
+          title: this.state.title,
+          description: this.state.description,
+          proposal: this.state.proposal,
+        }
+      });
+      // TODO: If success, redirect to Group Details (Wait for app bar support)
+      // this.setState({
+      //   redirect: `/group/${this.state.groupUuid}`,
+      //   push: false,
+      // });
+      // TODO: Remove. (Currently redirecting to Group Config)
+      window.history.back();
+    } catch (error) {  // If failed, set saving to false
+      this.setState({saving: false});
+    }
+  }
+
+  onCancel() {
+    window.history.back();
+  }
+
   render() {
+    // Check if redirect is needed
+    if (this.state.redirect) {
+      if (this.state.push) {
+        return (
+          <Redirect push to={this.state.redirect}/>
+        );
+      } else {
+        return (
+          <Redirect to={this.state.redirect}/>
+        );
+      }
+    }
+
     // App Bar
     let appBar = <AppBar/>;
+
+    if (this.state.error) {
+      return (
+        <React.Fragment>
+          {appBar}
+          <h1>Oops, something went wrong</h1>
+          <h3>Perhaps reload?</h3>
+        </React.Fragment>
+      );
+    }
+
+    if (this.state.loading) {
+      return (
+        <React.Fragment>
+          {appBar}
+          <LoadingOutlined/>
+        </React.Fragment>
+      );
+    }
 
     // Name (Group Owner) (Before Grouping DDL) or (Admin) (All Stages)
     let name = <React.Fragment>
@@ -79,7 +203,7 @@ export default class AccountProfile extends React.Component {
         <h1 className={'title'}>Group Name<span className={'required'}>*</span></h1>
         <Input className={'content'} onChange={this.onNameChange}
                value={this.state.name} maxLength={this.state.nameLimit}
-               disabled={!this.state.isAdmin && this.state.afterGroupingDDL}/>
+               disabled={this.state.isOwner && this.state.afterGroupingDDL}/>
       </div>
     </React.Fragment>
 
@@ -115,7 +239,7 @@ export default class AccountProfile extends React.Component {
     let Save = <React.Fragment>
       <div className={'gap'} />
       <div className={'gap'} />
-      <Button type={'primary'} block size={'large'}
+      <Button type={'primary'} block size={'large'} onClick={this.onSave}
               disabled={!this.state.name || !this.state.title}>
         Save
       </Button>
@@ -123,7 +247,7 @@ export default class AccountProfile extends React.Component {
 
     let Cancel = <React.Fragment>
       <div className={'gap'} />
-      <Button block size={'large'}>Cancel</Button>
+      <Button block size={'large'} onClick={this.onCancel}>Cancel</Button>
     </React.Fragment>;
 
     return (

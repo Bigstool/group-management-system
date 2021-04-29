e14f3b365abb6ea82e27d955bd6ac7732f706bb8
Bigstool <20697772+Bigstool@users.noreply.github.com>
2021-04-29 20:58:13 +0800

    Add routing to EditGroupProfile

--- src/web/src/index.js
+++ src/web/src/index.js
@@ -14,6 +14,7 @@ import {AuthProvider} from "./utilities/AuthProvider";
 import PrivateRoute from "./utilities/PrivateRoute";
 import HtmlHead from "./components/HtmlHead";
 import GroupDetails from "./pages/GroupDetails";
+import EditGroupProfile from './pages/EditGroupProfile';
 import AccountProfile from "./pages/AccountProfile";
 import GroupConfig from "./pages/GroupConfig";
 
@@ -26,6 +27,7 @@ ReactDOM.render((
                 <PrivateRoute path={"/user"} component={AccountProfile}/>
                 <Route path={"/login"} component={Login}/>
                 <PrivateRoute path={"/group/:uuid/config"} component={GroupConfig}/>
+                <PrivateRoute path={'/group/:uuid/edit'} component={EditGroupProfile}/>
                 <PrivateRoute path={"/group/:uuid"} component={GroupDetails}/>
                 <Route path={"*"} component={NotFound}/>
             </Switch>

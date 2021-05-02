91cff5747d9ecc4641c433740fe9ecac49aec250
Deleted User <16668056+nobody-65534@users.noreply.github.com>
2021-05-02 22:45:07 +0800

    GroupCard: style tweak

--- src/web/src/components/GroupCard.js
+++ src/web/src/components/GroupCard.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import {Card} from "antd";
-import {UsergroupAddOutlined} from "@ant-design/icons";
+import styles from "./GroupCard.scss";
+import {Card, Tag} from "antd";
+import {StarFilled, UsergroupAddOutlined} from "@ant-design/icons";
 import {Link} from "react-router-dom";
 
 export default class GroupCard extends React.PureComponent {
-    static propTypes = {
-        groupItem: PropTypes.object.isRequired
-    }
+  static propTypes = {
+    groupItem: PropTypes.object.isRequired,
+    highlight: PropTypes.bool
+  };
 
-    constructor(props) {
-        super(props);
-    }
+  static defaultProps = {
+    highlight: false
+  }
 
-    render() {
-        return (
-            <Link to={`/group/${this.props["groupItem"]["uuid"]}`}>
-                <Card>
-                    <Card.Meta
-                        title={this.props["groupItem"]["name"]}
-                        description={this.props["groupItem"]["description"]}
-                    />
-                </Card>
-            </Link>
+  constructor(props) {
+    super(props);
+  }
 
-        )
-    }
+  render() {
+    return (
+      <Link to={`/group/${this.props.groupItem.uuid}`}>
+        <Card className={`${styles.GroupCard} ${this.props.highlight && styles.Highlight}`} title={this.props.groupItem.title}
+              extra={this.props.groupItem.favorite && <StarFilled style={{color: 'orange'}}/>}
+        >
+          <Card.Meta
+            description={this.props.groupItem.description}
+          />
+          <div className={styles.GroupName}>
+            <UsergroupAddOutlined/>
+            <span className={styles.Name}>{this.props.groupItem.name}</span>
+            {this.props.highlight &&
+            <Tag className={styles.Tag} color={"processing"}>Your group</Tag>}
+          </div>
+        </Card>
+      </Link>
+
+    );
+  }
 }

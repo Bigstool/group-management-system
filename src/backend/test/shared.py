f0bebabdda59977d5ffca34923388e5ed9d676a7
4piu <16668056+4piu@users.noreply.github.com>
2021-05-12 01:19:16 +0800

    Pytest: merge tests into one

--- src/backend/test/shared.py
+++
@@ -1,9 +0,0 @@
-from logging import Logger
-from pprint import pformat
-
-import requests
-
-api = "http://localhost:8080"
-
-def log_res(logger:Logger, r: requests.Response):
-    logger.info('\n' + r.request.method + ' ' + r.url + '\n' + pformat(r.json()) + '\n')

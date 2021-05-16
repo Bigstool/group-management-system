5956b2359dde8b71d13fa80def3bd548d814f267
4piu <16668056+4piu@users.noreply.github.com>
2021-05-16 21:52:28 +0800

    Semester API: logout user when archive

--- src/backend/app/utility/Auth.py
+++ src/backend/app/utility/Auth.py
@@ -3,6 +3,8 @@ from utility.ApiException import *
 
 
 class Auth:
+    valid_after: float = None
+
     @staticmethod
     def get_payload(request):
         token = request.headers.get('Authorization')
@@ -13,4 +15,6 @@ class Auth:
             token_info = jwt_util.decode_token(token, audience='access')
         except:
             raise ApiPermissionException("Permission denied: invalid token")
+        if Auth.valid_after and token_info["role"] != "ADMIN" and token_info["iat"] < Auth.valid_after:
+            raise ApiPermissionException("Permission denied: expired token")
         return token_info
